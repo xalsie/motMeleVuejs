222ff7645f38a8e32d89f4a1044b5afbcde16665
alexis.giard <alexis.giard@habitatetjardin.fr>
2024-08-21 17:21:47 +0200

    chore: Add initial project files and configuration

---
+++ README.md
@@ -0,0 +1,16 @@
+# MotMeleeSolve
+
+## Technologies
+
+- Vue.js
+- Vite
+- Express
+- Tesseract.js
+
+## Description
+
+Ce projet est un site web où l'utilisateur peut poster une image de mots mêlés, et le backend se charge de résoudre le jeu.
+
+## Principe du Mot Mêlé
+
+Le principe du jeu de mots mêlés est de repérer, dans une grille remplie de lettres, des mots connus du dictionnaire, lus dans les deux sens, horizontalement, verticalement et en diagonale.
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import UploadForm from './components/UploadForm.vue';
+import DisplayImage from './components/DisplayImage.vue';
+import { ref } from 'vue';
+
+const imageUrl = ref('');
+
+function handleImageUpload(url) {
+    imageUrl.value = url;
+}
+</script>
+
+<template>
+    <div>
+        <!-- masque le composant UploadForm si une image a été importée -->
+        <UploadForm v-show="!imageUrl" @image-uploaded="handleImageUpload" />
+        <!-- affiche le composant DisplayImage si une image a été importée -->
+        <DisplayImage v-show="imageUrl" :imageUrl="imageUrl" />
+    </div>
+</template>
+
+<style scoped>
+/* Ajoutez vos styles ici */
+</style>
