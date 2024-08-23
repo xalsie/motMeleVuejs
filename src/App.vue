128659a72251adf63d919ccc901e353da5783838
alexis.giard <alexis.giard@habitatetjardin.fr>
2024-08-23 16:57:16 +0200

    feat: Add ResolveComponent for resolving mot melee - #1

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,38 @@
 import UploadForm from "./components/UploadForm.vue";
 import DisplayImage from "./components/DisplayImage.vue";
 import DisplayGrid from "./components/DisplayGrid.vue";
+import ResolveComponent from "./components/ResolveComponent.vue";
 import { ref } from "vue";
 </script>
 
 <template>
-    <div>
-        <UploadForm
-            v-show="!imageGridUrl"
-            :title="`Importer une photo de la grille`"
-            @image-imported="handleImageGridImported"
-            @grid-imported="handleGridImported"
-        />
+    <UploadForm
+        v-show="!imageGridUrl"
+        :title="`Importer une photo de la grille`"
+        @image-imported="handleImageGridImported"
+        @grid-imported="handleGridImported"
+    />
 
-        <div
-            style="display: flex; justify-content: center; align-items: center"
-        >
-            <DisplayImage v-show="imageGridUrl" :imageUrl="imageGridUrl" />
+    <div class="flex flex-row gap-4" v-show="!resolved">
+        <DisplayImage v-show="imageGridUrl" :imageUrl="imageGridUrl" />
 
-            <DisplayGrid v-show="imageGridUrl && grid" :grid="grid" />
-        </div>
-
-        <!-- TODO: detecte les mots a trouvé -->
-        <!-- <UploadForm
-            v-show="!imageWordsUrl"
-            :title="`Importer une photo des mots`"
-            @image-imported="handleWordsImported"
-        /> -->
-
-        <!-- <div style="display: flex; justify-content: center; align-items: center;">
-            <DisplayImage v-show="imageWordsUrl" :imageUrl="imageWordsUrl" /> -->
-
-        <!-- <DisplayGrid v-show="grid" :grid="grid" /> -->
-        <!-- </div> -->
-
-        <div>
-            <!-- resoudre -->
-            <button @click="resolve">Résoudre</button>
-        </div>
+        <DisplayGrid v-show="imageGridUrl && grid" :grid="grid" />
     </div>
+
+    <!-- TODO: detecte les mots a trouvé -->
+    <!-- <UploadForm
+        v-show="!imageWordsUrl"
+        :title="`Importer une photo des mots`"
+        @image-imported="handleWordsImported"
+    /> -->
+
+    <!-- <div style="display: flex; justify-content: center; align-items: center;">
+        <DisplayImage v-show="imageWordsUrl" :imageUrl="imageWordsUrl" /> -->
+
+    <!-- <DisplayGrid v-show="grid" :grid="grid" /> -->
+    <!-- </div> -->
+
+    <ResolveComponent v-show="grid && grid.length && words.length == 0" :grid="grid" :words="words" @resolved="handleGridResolved" @resetGrid="handleResetGrid" />
 </template>
 
 <script>
@@ -47,6 +41,7 @@ const imageGridUrl = ref("");
 const imageWordsUrl = ref("");
 const grid = ref([]);
 const words = ref([]);
+const resolved = ref(false);
 
 export default {
     components: {
@@ -54,7 +49,6 @@ export default {
     },
     methods: {
         handleImageGridImported(imageData) {
-            console.log("Image Grid imported:", imageData);
             imageGridUrl.value = imageData;
         },
         handleWordsImported(imageData) {
@@ -62,38 +56,18 @@ export default {
             // imageWordsUrl.value = imageData;
         },
         handleGridImported(gridData) {
-            console.log("Grid imported:", gridData);
             grid.value = gridData;
         },
-        async resolve() {
-            const response = await fetch("/resolve", {
-                method: "POST",
-                body: JSON.stringify({
-                    grid: grid.value,
-                    words: words.value,
-                }),
-                headers: { "Content-Type": "application/json" },
-                dataType: "json",
-            });
+        handleGridResolved(bool) {
+            resolved.value = bool;
+        },
+        handleResetGrid() {
+            imageGridUrl.value = "";
+            grid.value = [];
+            words.value = [];
+            resolved.value = false;
 
-            if (!response.ok) {
-                throw new Error("Erreur lors de l'importation de l'image.");
-            }
-
-            const result = await response.json();
-
-            // TODO: afficher les mots dans le DOM
-            const wordsFound = result.wordsResult;
-
-            const wordElement = document.createElement("div");
-            wordElement.innerHTML = wordsFound
-                .map(
-                    (word) =>
-                        `<div>${word.word} - ${word.positions.start.row}:${word.positions.start.col} - ${word.positions.end.row}:${word.positions.end.col}</div>`
-                )
-                .join("");
-
-            document.body.appendChild(wordElement);
+            console.log("Grid reseted");
         },
     },
 };
